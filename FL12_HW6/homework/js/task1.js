21b2b08ca815f5f35aaeb894c814a2f2801246cd
Andrei Positko <andrei.positko@chisw.com>
2019-12-26 10:04:31 +0200

    clear all errors

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,38 +1,38 @@
-let a = prompt("Please, input a");
+let a = prompt('Please, input a');
 if (isNaN(a) || !a || +a === 0) {
-  console.log("Invalid input data");
+  console.log('Invalid input data');
 } else {
-  console.log("a = " + a);
+  console.log('a = ' + a);
 }
 
-let b = prompt("Please, Input b");
+let b = prompt('Please, Input b');
 if (isNaN(b) || !b) {
-  console.log("Invalid input data");
+  console.log('Invalid input data');
 } else {
-  console.log("b = " + b);
+  console.log('b = ' + b);
 }
 
-let c = prompt("Please, Input c");
+let c = prompt('Please, Input c');
 if (isNaN(c) || !c) {
-  console.log("Invalid input data");
+  console.log('Invalid input data');
 } else {
-  console.log("c = " + c);
+  console.log('c = ' + c);
 }
 
 let x, x1, x2;
 
-console.log(a + "x^2 + " + b + "x + " + c + " = 0");
-let descr = b * b - 4 * a * c;
+console.log(a + 'x^2 + ' + b + 'x + ' + c + ' = 0');
+const descr = b * b - 4 * a * c;
 
 if (descr < 0) {
-  console.log("no solution (Descriminant < 0)");
-} else if (descr == 0) {
+  console.log('no solution (Descriminant < 0)');
+} else if (descr === 0) {
   x = Math.round(-b / (2 * a));
-  console.log("x = " + x);
+  console.log('x = ' + x);
 } else {
   x1 = Math.round((-b + Math.sqrt(descr)) / (2 * a));
   x2 = Math.round((-b - Math.sqrt(descr)) / (2 * a));
 
-  console.log("x1 = " + x1);
-  console.log("x2 = " + x2);
+  console.log('x1 = ' + x1);
+  console.log('x2 = ' + x2);
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,25 +1,25 @@
-let a = +prompt("Please, enter the first side (a) of triangle");
-let b = +prompt("Please, enter the second side (b) of triangle");
-let c = +prompt("Please, enter the third side (c) of triangle");
+let a = +prompt('Please, enter the first side (a) of triangle');
+let b = +prompt('Please, enter the second side (b) of triangle');
+let c = +prompt('Please, enter the third side (c) of triangle');
 
 if (
-  a === "" ||
-  b === "" ||
-  c === "" ||
-  eslint(a) !== a ||
+  a === '' ||
+  b === '' ||
+  c === '' ||
+  parseInt(a) !== a ||
   parseInt(b) !== b ||
   parseInt(c) !== c
 ) {
-  alert("input values shoud be ONLY numbers");
+  alert('input values shoud be ONLY numbers');
 } else if (a <= 0 || b <= 0 || c <= 0) {
-  alert("A triangle must have 3 sides with a positive definite length");
+  alert('A triangle must have 3 sides with a positive definite length');
 } else if (!(a + b > c || b + c > a || c + a > b)) {
-  alert("Triangle doesn’t exist");
-  console.log("Triangle doesn’t exist");
+  alert('Triangle doesn’t exist');
+  console.log('Triangle doesn’t exist');
 } else if (a === b && b === c) {
-  console.log("Equilateral triangle");
+  console.log('Equilateral triangle');
 } else if (a === b || b === c || c === a) {
-  console.log("Isosceles triangle");
+  console.log('Isosceles triangle');
 } else {
-  console.log("Scalene triangle");
+  console.log('Scalene triangle');
 }
